1306499c0bdfd557723a9d27e45271901b0cbc44
Nathandiv <k.n.hlungwani@gmail.com>
2025-01-10 16:10:15 +0200

    added navbar in all pages

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../UI/Shared-UI/navbar/navbar.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
--- src/app/pages/apply/apply.component.ts
+++ src/app/pages/apply/apply.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../UI/Shared-UI/navbar/navbar.component';
 
 @Component({
   selector: 'app-apply',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './apply.component.html',
   styleUrl: './apply.component.css'
 })
