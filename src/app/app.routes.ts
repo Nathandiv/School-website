de6f75f4059b04832fb634df6db59250c0875728
Nathandiv <k.n.hlungwani@gmail.com>
2025-01-10 15:57:35 +0200

    added routings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    {
+        path: "",
+        title: "home",
+        component: HomeComponent,
+
+    },
+
+
+    {
+        path:"contact",
+        title: "contact",
+        component: ContactComponent,
+
+    },
+    {
+        path: "about",
+        title:"about",
+        component: AboutComponent,
+    },
+
+
+    // {
+    //     path:"**",
+    //     title: "error",
+    //     component: ErrorComponent,
+    //  },
+
+
+];
