b14abed32d2913a9290b0736a38002248b64e980
Nathandiv <k.n.hlungwani@gmail.com>
2025-01-10 16:01:39 +0200

    added navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { AboutComponent } from './pages/about/about.component';
+import { ApplyComponent } from './pages/apply/apply.component';
 
 export const routes: Routes = [
     
@@ -23,6 +24,12 @@ export const routes: Routes = [
         path: "about",
         title:"about",
         component: AboutComponent,
+    },
+
+    {
+        path: "about",
+        title:"about",
+        component: ApplyComponent,
     },
 
 
