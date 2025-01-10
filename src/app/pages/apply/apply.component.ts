fb7d749accac0b027b93a332aba38f7269122eea
Nathandiv <k.n.hlungwani@gmail.com>
2025-01-10 16:14:33 +0200

    added footer on apply page

--- src/app/pages/apply/apply.component.ts
+++ src/app/pages/apply/apply.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../UI/Shared-UI/navbar/navbar.component';
+import { FooterComponent } from '../../UI/Shared-UI/footer/footer.component';
 
 @Component({
   selector: 'app-apply',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,FooterComponent],
   templateUrl: './apply.component.html',
   styleUrl: './apply.component.css'
 })
