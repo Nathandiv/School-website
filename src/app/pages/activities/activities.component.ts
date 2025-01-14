37a31b408468d9e2d87d29c771c3d2b268eba218
Nathandiv <k.n.hlungwani@gmail.com>
2025-01-14 13:49:18 +0200

    added footer and navbar on activities

--- src/app/pages/activities/activities.component.ts
+++ src/app/pages/activities/activities.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../UI/Shared-UI/footer/footer.component';
+import { NavbarComponent } from '../../UI/Shared-UI/navbar/navbar.component';
 
 @Component({
   selector: 'app-activities',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent,FooterComponent],
   templateUrl: './activities.component.html',
   styleUrl: './activities.component.css'
 })
