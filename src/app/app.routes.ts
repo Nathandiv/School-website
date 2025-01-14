a2ae35796694b61d54ac39b1b8675a7e345b10c6
Nathandiv <k.n.hlungwani@gmail.com>
2025-01-14 13:47:10 +0200

    added rooting for the missing pages and also created them

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ApplyComponent } from './pages/apply/apply.component';
+import { ArchiversComponent } from './pages/archivers/archivers.component';
+import { ActivitiesComponent } from './pages/activities/activities.component';
+import { ErrorComponent } from './pages/error/error.component';
 
 export const routes: Routes = [
     
@@ -27,17 +30,30 @@ export const routes: Routes = [
     },
 
     {
-        path: "about",
-        title:"about",
+        path: "archivers",
+        title:"archivers",
+        component: ArchiversComponent,
+    },
+
+    {
+        path: "activities",
+        title:"activities",
+        component: ActivitiesComponent,
+    },
+
+    {
+        path: "Apply",
+        title:"Apply",
         component: ApplyComponent,
     },
 
 
-    // {
-    //     path:"**",
-    //     title: "error",
-    //     component: ErrorComponent,
-    //  },
+
+    {
+        path:"**",
+        title: "error",
+        component: ErrorComponent,
+     },
 
 
 ];
