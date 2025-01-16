d97d7e0754d132eef9874cd1c9ac05db4d389313
Nathandiv <k.n.hlungwani@gmail.com>
2025-01-16 11:27:24 +0200

    added logic on navbar

--- src/app/UI/Shared-UI/navbar/navbar.component.ts
+++ src/app/UI/Shared-UI/navbar/navbar.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  isDropdownVisible = false;
 
-  
+  showDropdown() {
+    this.isDropdownVisible = true;
+  }
 
+  hideDropdown() {
+    this.isDropdownVisible = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const targetElement = event.target as HTMLElement;
+    if (!targetElement.closest('.group')) {
+      this.isDropdownVisible = false;
+    }
+  }
 }
