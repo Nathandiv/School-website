c95441c97857929757ba78a144b092636c7a9e99
Nathandiv <nathan.hlungwani123@gmail.com>
2025-01-17 20:31:26 +0200

    added govern component

---
+++ src/app/pages/govern/govern.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-govern',
+  standalone: true,
+  imports: [],
+  templateUrl: './govern.component.html',
+  styleUrl: './govern.component.css'
+})
+export class GovernComponent {
+
+}
