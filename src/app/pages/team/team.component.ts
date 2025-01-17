650359dbb5c7fb87faac4b0bbd4a30ea25df9816
Nathandiv <nathan.hlungwani123@gmail.com>
2025-01-17 21:00:07 +0200

    added the team image card

--- src/app/pages/team/team.component.ts
+++ src/app/pages/team/team.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../UI/Shared-UI/navbar/navbar.component';
+import { FooterComponent } from '../../UI/Shared-UI/footer/footer.component';
 
 @Component({
   selector: 'app-team',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent,FooterComponent],
   templateUrl: './team.component.html',
   styleUrl: './team.component.css'
 })
--- src/app/pages/vacancies/vacancies.component.ts
+++ src/app/pages/vacancies/vacancies.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../UI/Shared-UI/navbar/navbar.component";
+import { FooterComponent } from "../../UI/Shared-UI/footer/footer.component";
 
 @Component({
   selector: 'app-vacancies',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './vacancies.component.html',
   styleUrl: './vacancies.component.css'
 })
