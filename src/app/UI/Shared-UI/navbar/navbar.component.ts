e9d165c9d8f1d53fb7fd89ab544453b6e5b261d0
Nathandiv <nathan.hlungwani123@gmail.com>
2025-01-25 21:58:44 +0200

    made chanes on navbar

--- src/app/UI/Shared-UI/navbar/navbar.component.ts
+++ src/app/UI/Shared-UI/navbar/navbar.component.ts
@@ -9,21 +9,41 @@ import { RouterLink,RouterModule } from '@angular/router';
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-  isDropdownVisible = false;
 
+  isDropdownVisible = false; // "About Us" dropdown visibility
+  isAdmissionDropdownVisible = false; // "Admission" dropdown visibility
+
+  // Show "About us" dropdown
   showDropdown() {
+    this.isAdmissionDropdownVisible = false; // Close "Admission" dropdown if open
     this.isDropdownVisible = true;
   }
 
+  // Hide "About us" dropdown
   hideDropdown() {
     this.isDropdownVisible = false;
+  }
+
+  // Show "Admission" dropdown
+  showAdmissionDropdown() {
+    this.isDropdownVisible = false; // Close "About us" dropdown if open
+    this.isAdmissionDropdownVisible = true;
+  }
+
+  // Hide "Admission" dropdown
+  hideAdmissionDropdown() {
+    this.isAdmissionDropdownVisible = false;
   }
 
   @HostListener('document:click', ['$event'])
   onDocumentClick(event: MouseEvent) {
     const targetElement = event.target as HTMLElement;
-    if (!targetElement.closest('.group')) {
+
+    // Close dropdowns if clicked outside
+    if (!targetElement.closest('.about-dropdown') && !targetElement.closest('.admission-dropdown')) {
       this.isDropdownVisible = false;
+      this.isAdmissionDropdownVisible = false;
     }
   }
+
 }
