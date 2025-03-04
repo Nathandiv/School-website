97d695087375ffbb47fa3d3cd39c518418a8196d
Nathandiv <nathan.hlungwani123@gmail.com>
2025-03-04 20:21:32 +0200

    added new navbar to work on

--- src/app/UI/Shared-UI/navbar/navbar.component.ts
+++ src/app/UI/Shared-UI/navbar/navbar.component.ts
@@ -10,40 +10,40 @@ import { RouterLink,RouterModule } from '@angular/router';
 })
 export class NavbarComponent {
 
-  isDropdownVisible = false; // "About Us" dropdown visibility
-  isAdmissionDropdownVisible = false; // "Admission" dropdown visibility
+  // isDropdownVisible = false; // "About Us" dropdown visibility
+  // isAdmissionDropdownVisible = false; // "Admission" dropdown visibility
 
-  // Show "About us" dropdown
-  showDropdown() {
-    this.isAdmissionDropdownVisible = false; // Close "Admission" dropdown if open
-    this.isDropdownVisible = true;
-  }
+  // // Show "About us" dropdown
+  // showDropdown() {
+  //   this.isAdmissionDropdownVisible = false; // Close "Admission" dropdown if open
+  //   this.isDropdownVisible = true;
+  // }
 
-  // Hide "About us" dropdown
-  hideDropdown() {
-    this.isDropdownVisible = false;
-  }
+  // // Hide "About us" dropdown
+  // hideDropdown() {
+  //   this.isDropdownVisible = false;
+  // }
 
-  // Show "Admission" dropdown
-  showAdmissionDropdown() {
-    this.isDropdownVisible = false; // Close "About us" dropdown if open
-    this.isAdmissionDropdownVisible = true;
-  }
+  // // Show "Admission" dropdown
+  // showAdmissionDropdown() {
+  //   this.isDropdownVisible = false; // Close "About us" dropdown if open
+  //   this.isAdmissionDropdownVisible = true;
+  // }
 
-  // Hide "Admission" dropdown
-  hideAdmissionDropdown() {
-    this.isAdmissionDropdownVisible = false;
-  }
+  // // Hide "Admission" dropdown
+  // hideAdmissionDropdown() {
+  //   this.isAdmissionDropdownVisible = false;
+  // }
 
-  @HostListener('document:click', ['$event'])
-  onDocumentClick(event: MouseEvent) {
-    const targetElement = event.target as HTMLElement;
+  // @HostListener('document:click', ['$event'])
+  // onDocumentClick(event: MouseEvent) {
+  //   const targetElement = event.target as HTMLElement;
 
-    // Close dropdowns if clicked outside
-    if (!targetElement.closest('.about-dropdown') && !targetElement.closest('.admission-dropdown')) {
-      this.isDropdownVisible = false;
-      this.isAdmissionDropdownVisible = false;
-    }
-  }
+  //   // Close dropdowns if clicked outside
+  //   if (!targetElement.closest('.about-dropdown') && !targetElement.closest('.admission-dropdown')) {
+  //     this.isDropdownVisible = false;
+  //     this.isAdmissionDropdownVisible = false;
+  //   }
+  // }
 
 }
